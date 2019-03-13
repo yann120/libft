6e5729756a3c75f9666a71f9d97a891f20ca4353
Yann PETITJEAN <ypetitje@e1r8p3.42.fr>
2019-03-13 14:13:42 +0100

    strncmp now with size_t

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,28 +6,26 @@
 /*   By: ypetitje <ypetitje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 09:48:50 by ypetitje          #+#    #+#             */
-/*   Updated: 2018/11/21 17:31:27 by ypetitje         ###   ########.fr       */
+/*   Updated: 2019/03/13 14:13:26 by ypetitje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *str1, const char *str2, int n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
-	int stop;
+	size_t i;
 
 	i = 0;
-	stop = 0;
-	while (str1[i] && stop == 0 && i < n)
+	while (s1[i] && i < n)
 	{
-		if (str1[i] != str2[i])
-			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if (str1[0] == 0)
-		return (-str2[0]);
-	else if (str1[i] == 0 && i < n)
-		return (-str2[i]);
+	if (s1[0] == 0)
+		return (-s2[0]);
+	else if (s1[i] == 0 && i < n)
+		return (-s2[i]);
 	return (0);
 }
